0d8df76873cf0e919b45efba111b702b13147b68
Dave <dlp140@gmail.com>
2022-07-11 12:47:45 -0400

    added Photo model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Record, Collection
+from .models import Record, Collection, Photo
 
 # Register your models here.
 admin.site.register(Record)
 admin.site.register(Collection)
+admin.site.register(Photo)
