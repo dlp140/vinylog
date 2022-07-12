f6e2802f800231e488cd8575218807e52464f339
Dave <dlp140@gmail.com>
2022-07-12 15:09:15 -0400

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vinylog.wsgi
--- vinylog/settings.py
+++ vinylog/settings.py
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/records/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
